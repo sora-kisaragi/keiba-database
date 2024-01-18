3a6e15dc733b5d77b78e9e335b1fb4b3c45c964a
匿名お嬢様 <secret.gentle.human@gmail.com>
2024-01-18 16:24:42 +0900

    feat: 騎手IDのリストを取得して保管する機能を追加

--- src/main.py
+++ src/main.py
@@ -3,28 +3,31 @@ from scraper.horce_scraper import HorseScraper
 import pandas as pd
 import argparse
 
+
 def jockey_main(jockey_id, scraping, skip_scraping):
-    url = "https://db.netkeiba.com/"
-    scraper = JockeyScraper(url)
-    print(f'騎手ID: {jockey_id}')
-    print(f'スクレイピングフラグ: {scraping}')
-    data = scraper.get_jockey_data(jockey_id, scraping=scraping)
-    # keyをソートしてdataを表示
-    sorted_data = sorted(data.items(), key=lambda x: x[0])
-    for key, value in sorted_data:
-        print(f'{key}着', f'{value}回')
-    # valueの合計を計算して表示
-    total = sum(data.values())
-    print('total:', total)
+    scraper = JockeyScraper()
+    # print(f'騎手ID: {jockey_id}')
+    # print(f'スクレイピングフラグ: {scraping}')
+    # data = scraper.get_jockey_data(jockey_id, scraping=scraping)
+    # # keyをソートしてdataを表示
+    # sorted_data = sorted(data.items(), key=lambda x: x[0])
+    # for key, value in sorted_data:
+    #     print(f'{key}着', f'{value}回')
+    # # valueの合計を計算して表示
+    # total = sum(data.values())
+    # print('total:', total)
 
-    new_data = combine_values(dict(sorted_data))
+    # new_data = combine_values(dict(sorted_data))
 
-    # new_dataの着順の平均を計算して表示
-    x = 0
-    for key, value in new_data.items():
-        x += int(key) * int(value)
-    print('騎手名 : ', scraper.jockey_name)
-    print('平均順位 : ', x / total)
+    # # new_dataの着順の平均を計算して表示
+    # x = 0
+    # for key, value in new_data.items():
+    #     x += int(key) * int(value)
+    # print('騎手名 : ', scraper.jockey_name)
+    # print('平均順位 : ', x / total)
+    print('騎手IDをリストで取得を開始する')
+    scraper.scrape_all_jockey_idlist(update=True)
+
 
 def horse_main(horse_id, scraping, skip_scraping):
     scraper = HorseScraper()
@@ -50,6 +53,7 @@ def horse_main(horse_id, scraping, skip_scraping):
     print(f'馬名: {data["profile"].iloc[0]["馬名"]}')
     print(data['race_results'][['騎手', '着 順']])
 
+
 def combine_values(d):
     keys_to_remove = []
     for key in d:
@@ -62,9 +66,10 @@ def combine_values(d):
         del d[key]
     return d
 
+
 def str2bool(v):
     if isinstance(v, bool):
-       return v
+        return v
     if v.lower() in ('yes', 'true', 't', 'y', '1'):
         return True
     elif v.lower() in ('no', 'false', 'f', 'n', '0'):
@@ -76,13 +81,16 @@ def str2bool(v):
 # コマンドライン引数から騎手IDを取得
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('--jockey_id', type=str, default="00666", help='騎手IDを入力してください default: 00666 武豊')
-    parser.add_argument('--horse_id', type=str, default="2017105319", help='馬IDを入力してください default: 2017105319 エフフォーリア')
-    parser.add_argument('--no_scraping', action='store_false', help='スクレイピングを行わない場合に指定 default: True')
-    parser.add_argument('--skip_scraping', action='store_true', help='データが存在する場合スクレイピングをスキップする場合に指定 default: False')
-
+    parser.add_argument('--jockey_id', type=str, default="00666",
+                        help='騎手IDを入力してください default: 00666 武豊')
+    parser.add_argument('--horse_id', type=str, default="2017105319",
+                        help='馬IDを入力してください default: 2017105319 エフフォーリア')
+    parser.add_argument('--no_scraping', action='store_false',
+                        help='スクレイピングを行わない場合に指定 default: True')
+    parser.add_argument('--skip_scraping', action='store_true',
+                        help='データが存在する場合スクレイピングをスキップする場合に指定 default: False')
 
     args = parser.parse_args()
 
-    # jockey_main(args.jockey_id, args.no_scraping, args.skip_scraping)
-    horse_main(args.horse_id, args.no_scraping, args.skip_scraping)
+    jockey_main(args.jockey_id, args.no_scraping, args.skip_scraping)
+    # horse_main(args.horse_id, args.no_scraping, args.skip_scraping)
--- src/scraper/jockey_scraper.py
+++ src/scraper/jockey_scraper.py
@@ -1,20 +1,25 @@
+import re
 import requests
 import math
-import time
-import sys
 import os
 import pandas as pd
 from tqdm import tqdm
 from bs4 import BeautifulSoup
-from utils import html_parser
+
 
 class JockeyScraper:
-    def __init__(self, base_url):
-        self.base_url = base_url
+    def __init__(self):
+        self.base_url = 'https://db.netkeiba.com/'
         self.list_url = "https://db.netkeiba.com/jockey/"
         self.jockey_name = None
+        # param_1 : 現役 or 引退 [0 or 1]
+        # param_2 : ページ数
+        self.jockey_db_url = 'https://db.netkeiba.com//?pid=jockey_list&list=100&act%5B0%5D={}&page={}'
+        # param : スクレイピングを実行した日付
+        self.jockey_id_list_file = '../data/jockey/jockey_id_list/{}.csv'
+        self.last_page_file = '../data/jockey/last_page_{}.txt'
 
-    def get_jockey_data(self, jockey_id,scraping=True):
+    def get_jockey_data(self, jockey_id, scraping=True):
         data, self.jockey_name = self.get_jockey_record_totals(jockey_id)
         # コンマを削除してから整数に変換
         num = int(data['着外'].replace(',', ''))
@@ -33,7 +38,8 @@ class JockeyScraper:
     def _get_jockey_name(self, soup):
         head_div = soup.find('div', class_='db_head_name fc')
         name_div = head_div.find('div', class_='Name')
-        name = name_div.h1.get_text(strip=True).replace(' ', '').replace('.', '. ')
+        name = name_div.h1.get_text(strip=True).replace(
+            ' ', '').replace('.', '. ')
         return name.replace('\n', ' ')
 
     def get_jockey_record_totals(self, jockey_id):
@@ -58,13 +64,189 @@ class JockeyScraper:
         for row in rows:
             if row.find('td', class_='cel_opacity'):  # 'cel_opacity'クラスを持つ要素が存在する場合のみ処理を行う
                 row_data['total'] = row.find('td', class_='cel_opacity').text
-                row_data['1着'] = row.find('a', href=lambda x: x and 'mode=r1' in x).text if row.find('a', href=lambda x: x and 'mode=r1' in x) else None
-                row_data['2着'] = row.find('a', href=lambda x: x and 'mode=r2' in x).text if row.find('a', href=lambda x: x and 'mode=r2' in x) else None
-                row_data['3着'] = row.find('a', href=lambda x: x and 'mode=r3' in x).text if row.find('a', href=lambda x: x and 'mode=r3' in x) else None
-                row_data['着外'] = row.find('a', href=lambda x: x and 'mode=r4' in x).text if row.find('a', href=lambda x: x and 'mode=r4' in x) else None
-                break # 初回のみ処理を行う
+                row_data['1着'] = row.find('a', href=lambda x: x and 'mode=r1' in x).text if row.find(
+                    'a', href=lambda x: x and 'mode=r1' in x) else None
+                row_data['2着'] = row.find('a', href=lambda x: x and 'mode=r2' in x).text if row.find(
+                    'a', href=lambda x: x and 'mode=r2' in x) else None
+                row_data['3着'] = row.find('a', href=lambda x: x and 'mode=r3' in x).text if row.find(
+                    'a', href=lambda x: x and 'mode=r3' in x) else None
+                row_data['着外'] = row.find('a', href=lambda x: x and 'mode=r4' in x).text if row.find(
+                    'a', href=lambda x: x and 'mode=r4' in x) else None
+                break  # 初回のみ処理を行う
 
-        return row_data , jockey_name
+        return row_data, jockey_name
+
+    def scrape_jockey_idlist(self, active=1, update=False):
+        '''
+        騎手のIDリストを取得する。
+
+        Parameters:
+            active (int): 騎手の状態を指定します。0は引退、1は現役です。
+            update (bool): データを更新する場合はTrueを指定します。
+
+        Returns:
+            list: 騎手のIDと名前のリスト。
+
+        '''
+        # もし前回の処理が中断していた場合は続きから処理を行う
+        if os.path.exists(self.last_page_file.format(active)) and not update:
+            with open(self.last_page_file.format(active)) as f:
+                # 前回処理したページのURLを取得
+                last_page_url = f.read()
+            # 前回処理したページのURLからページ数を取得
+            last_page = int(
+                re.search(r'page=([0-9]+)', last_page_url).group(1))
+            # 現在のページ数を表示
+            print(f'前回処理したページ: {last_page}')
+        else:
+            # 前回処理が中断していない場合は1ページ目から処理を開始
+            last_page = 1
+
+        # URLを作成
+        url = self.jockey_db_url.format(active, last_page)
+
+        # 処理時間をバーで表示するための設定
+        try:
+            response = requests.get(url)
+            response.encoding = 'euc-jp'  # エンコーディングをeuc-jpに設定
+            soup = BeautifulSoup(response.text, 'html.parser')
+            pager = soup.find('div', class_='common_pager')
+            last_url = pager.find('a', title="最後").get('href')
+            max_page = int(re.search(r'page=([0-9]+)', last_url).group(1))
+        except Exception as e:
+            print(f'最大ページ数を取得中にエラーが発生しました。{e}')
+            return False
+
+        with tqdm(total=max_page) as pbar:
+            try:
+                # 開始時刻を取得
+                start_time = pd.Timestamp.now()
+
+                # 保存するファイル名を作成
+                list_file = self.jockey_id_list_file.format(
+                    pd.Timestamp.now().strftime('%Y%m%d'))
+
+                # 保存ディレクトリが存在しない場合は作成
+                if not os.path.exists(os.path.dirname(list_file)):
+                    print(f'{os.path.dirname(list_file)}を作成します。')
+                    os.makedirs(os.path.dirname(list_file))
+
+                # ファイルが存在していない場合はヘッダーを付ける
+                if not os.path.exists(list_file):
+                    pd.DataFrame(columns=[
+                        'jockey_id', 'jockey_name', 'active']).to_csv(list_file)
+
+                # ファイルが存在しupdateする場合は保管しているリストを削除
+                elif os.path.exists(list_file) and update:
+                    if os.path.exists(list_file):
+                        os.remove(list_file)
+
+                while True:
+                    # 開始時刻からの経過時間を取得
+                    elapsed_time = pd.Timestamp.now() - start_time
+                    # 経過時間が5分を超えた場合は処理を中断
+                    if elapsed_time.seconds > 300:
+                        raise Exception('処理がタイムアウトしました。')
+                    # URLからHTMLを取得
+                    response = requests.get(url)
+                    response.encoding = 'euc-jp'  # エンコーディングをeuc-jpに設定
+                    response.raise_for_status()  # ステータスコードが200以外の場合に例外を発生させる
+
+                    # HTMLをパース
+                    soup = BeautifulSoup(response.text, 'html.parser')
+
+                    # pagerを取得
+                    pager = soup.find('div', class_='common_pager')
+
+                    # 次のページへのリンクがある場合はURLを取得
+                    if pager.find('a', title='次'):
+                        scrape_end = False
+                        next_page_url = pager.find(
+                            'a', title='次')['href']
+                    else:
+                        # 次のページへのリンクがない場合はbreak
+                        scrape_end = True
+
+                    # 表を取得
+                    table = soup.find(
+                        'table', class_='nk_tb_common race_table_01')
+
+                    # 表から騎手のIDと名前を取得
+
+                    # リスト内包表記を使わない場合は以下のように書く
+                    # 実行速度計測のため
+                    jockey_data = []
+                    rows = table.find_all('tr')
+                    for row in rows:
+                        columns = row.find_all('td')
+                        if len(columns) > 0:
+                            # 騎手のIDをhrefから取得
+                            jockey_id = columns[0].find('a').get('href').replace(
+                                '/jockey/result/recent/', '').strip('/')
+                            # 騎手の名前を取得
+                            jockey_name = columns[0].text
+                            # 騎手のIDと名前と現役かどうかをタプルにしてリストに追加
+                            jockey_data.append(
+                                (jockey_id, jockey_name, active))
+
+                    # リスト内包表記を使って書くと以下のようになる
+                    # 実行速度計測のためにコメントアウト
+                    # jockey_data = [(columns[0].find('a').get('href').replace('/jockey/result/recent/', '').strip('/'), columns[0].text)
+                    #                for row in table.find_all('tr') for columns in [row.find_all('td')] if len(columns) > 0]
+
+                    # データをcsv形式で保存
+                    # 存在チェックはwhile前に行う
+                    pd.DataFrame(jockey_data).to_csv(
+                        list_file, header=False, mode='a')
+
+                    # バーを更新
+                    pbar.update(1)
+
+                    # breakフラグが立っていた場合はループを抜ける
+                    if scrape_end:
+                        break
+                    else:
+                        # 次のページのURLを作成
+                        url = next_page_url
+
+            except Exception as e:
+                print(e)
+                print('処理を中断します。')
+
+                # 次のページURLを保管し続きから再開できるようにする
+                # ファイルの存在チェックをする
+                if os.path.exists(self.last_page_file.format(active)):
+                    # ファイルが存在している場合は上書き
+                    with open(self.last_page_file.format(active), 'w') as f:
+                        f.write(next_page_url)
+                else:
+                    # ファイルが存在していない場合は新規作成
+                    with open(self.last_page_file.format(active), 'x') as f:
+                        f.write(next_page_url)
+
+            else:
+                # 保管しているページ数を削除
+                if os.path.exists(self.last_page_file.format(active)):
+                    os.remove(self.last_page_file.format(active))
+
+            finally:
+                # 取得したデータを返す
+                jockey_data_list = pd.read_csv(list_file)
+                return jockey_data_list
+
+    def scrape_all_jockey_idlist(self, update=False):
+        '''
+            現役、引退両方の騎手のIDリストを取得する。
+            return: 成功したらTrue、失敗したらFalseを返す。
+        '''
+        try:
+            # 最初にUpdateを行い、二回目の実行時は追加を行うことでクリーンなファイルとする
+            self.scrape_jockey_idlist(active=0, update=update)
+            self.scrape_jockey_idlist(active=1)
+        except Exception as e:
+            print(f'エラーが発生しました。{e}')
+            return False
+        return True
 
     def scrape_jockey_list(self):
         # URLを作成
@@ -92,7 +274,8 @@ class JockeyScraper:
         response = requests.get(url)
 
         # レスポンスのエンコーディングを確認
-        encoding = response.encoding if 'charset' in response.headers.get('content-type', '').lower() else None
+        encoding = response.encoding if 'charset' in response.headers.get(
+            'content-type', '').lower() else None
         # レスポンスをバイナリデータとして取得し、適切なエンコーディングでデコード
         html = response.content.decode(encoding if encoding else 'euc-jp')
 
@@ -120,7 +303,7 @@ class JockeyScraper:
             # 指定されたページ数だけループを回す
             for page in range(1, num_pages + 1):
                 # scrape_jockey_dataメソッドを呼び出す
-                page_data = self.scrape_jockey_data(jockey_id,page)
+                page_data = self.scrape_jockey_data(jockey_id, page)
                 # データを格納
                 data[page] = page_data
                 pbar.update(1)
